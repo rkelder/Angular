c8a191aa06f597c3722c6a3add59cbe53ad51bd2
Roderik Kelder <130762837+rkelder@users.noreply.github.com>
2023-09-11 13:14:52 +0200

    My First Project

---
+++ my-first-project/src/app/server/server.component.ts
@@ -0,0 +1,16 @@
+// decorator: Typescript feature that enhances elements that you use in your code.
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+})
+export class ServerComponent {
+  serverId: number = 10;
+  serverStatus: string = 'offline';
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+}
