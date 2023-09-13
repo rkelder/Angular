195a0710fb04cd743a4851ef6a6613b38cb046d5
rkelder <roderik.kelder@gmail.com>
2023-09-13 14:20:08 +0200

    Changes

--- angular_thecompleteguide/src/app/recipes/recipelist/recipelist.component.ts
+++ angular_thecompleteguide/src/app/recipes/recipelist/recipelist.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipelist',
   templateUrl: './recipelist.component.html',
   styleUrls: ['./recipelist.component.css']
 })
-export class RecipelistComponent {
+export class RecipelistComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simply a test', 'https://tastesbetterfromscratch.com/wp-content/uploads/2020/04/Massaman-Curry-1.jpg'), 
+  ]
+
+  constructor() {}
+
+  ngOnInit() {
+  }
 
 }
--- angular_thecompleteguide/src/app/shoppinglist/shoppinglist.component.ts
+++ angular_thecompleteguide/src/app/shoppinglist/shoppinglist.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shoppinglist',
   templateUrl: './shoppinglist.component.html',
   styleUrls: ['./shoppinglist.component.css']
 })
-export class ShoppinglistComponent {
+export class ShoppinglistComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
+
+  constructor() {}
+
+ngOnInit() {
+}
 
 }
