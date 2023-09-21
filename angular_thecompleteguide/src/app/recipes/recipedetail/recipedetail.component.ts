17e49133e6b2ca35019937e0a71ff566888431f4
rkelder <roderik.kelder@gmail.com>
2023-09-21 15:33:01 +0200

    Recipes

--- angular_thecompleteguide/src/app/recipes/recipedetail/recipedetail.component.ts
+++ angular_thecompleteguide/src/app/recipes/recipedetail/recipedetail.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipedetail',
   templateUrl: './recipedetail.component.html',
-  styleUrls: ['./recipedetail.component.css']
+  styleUrls: ['./recipedetail.component.css'],
 })
-export class RecipedetailComponent {
+export class RecipedetailComponent implements OnInit {
+  @Input()
+  recipe!: Recipe;
 
+  constructor() {}
+  ngOnInit() {}
 }
--- angular_thecompleteguide/src/app/recipes/recipelist/recipeitem/recipeitem.component.ts
+++ angular_thecompleteguide/src/app/recipes/recipelist/recipeitem/recipeitem.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipeitem',
   templateUrl: './recipeitem.component.html',
-  styleUrls: ['./recipeitem.component.css']
+  styleUrls: ['./recipeitem.component.css'],
 })
-export class RecipeitemComponent {
+export class RecipeitemComponent implements OnInit {
+  @Input()
+  recipe: Recipe = { name: '', description: '', imagePath: '' };
 
+  @Output() recipeSelected = new EventEmitter<void>();
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onSelected() {
+    this.recipeSelected.emit();
+  }
 }
